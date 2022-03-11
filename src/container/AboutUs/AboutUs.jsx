af85d845d3ac165d5ddcefdce0aed4932c50347a
BINK <bink@email.ua>
2022-03-11 19:08:45 +0200

    add section about

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -16,7 +16,7 @@ const AboutUs = () => (
     <div className="app__aboutus-content  flex__center">
       <div className="app__aboutus-content_about">
         <h1 className="headtext__cormorant">About Us</h1>
-        <img src={images.spoon} alt="about_spoon" className="spoon__img" />
+        <img src={images.spoon} alt="about_spoon" className="spoon__img left" />
         <p className="p__opensans">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra
           adipiscing ultrices vulputate posuere tristique. In sed odio nec
@@ -29,6 +29,19 @@ const AboutUs = () => (
 
       <div className="app__aboutus-content_knife flex__center">
         <img src={images.knife} alt="about__knife" />
+      </div>
+
+      <div className="app__aboutus-content_history">
+        <h1 className="headtext__cormorant">Our History</h1>
+        <img src={images.spoon} alt="about_spoon" className="spoon__img" />
+        <p className="p__opensans">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quis pharetra
+          adipiscing ultrices vulputate posuere tristique. In sed odio nec
+          aliquet eu proin mauris et.
+        </p>
+        <button type="button" className="custom__button">
+          Know More
+        </button>
       </div>
     </div>
   </div>
